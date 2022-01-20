437a2f2aafb05ff04c762bb6752aad31437cd3ea
Ramos <joao.lucas.c.ramos@accenture.com>
2022-01-20 01:09:11 -0300

    Adicionando uma classe para funcionários

--- src/camibank/Cliente.java
+++ src/camibank/Cliente.java
@@ -4,29 +4,27 @@ public class Cliente {
 	private String nome;
 	private String cpf;
 	private String profissao;
+	
+	public Cliente(String nome, String cpf, String profissao) {
+		if(nome.isEmpty()|| cpf.isEmpty() || profissao.isEmpty()) {
+			System.err.println("CAMPO OBRIGATÓRIO NULO");
+			return;
+		}
+		this.nome = nome;
+		this.cpf = cpf;
+		this.profissao = profissao;
+	}
 
 	public String getNome() {
 		return this.nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
 	}
 	
 	public String getCpf() {
 		return this.cpf;
 	}
 	
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	
 	public String getProfissao() {
 		return this.profissao;
-	}
-	
-	public void setProfissao(String profissao) {
-		this.profissao = profissao;
 	}
 	
 }
--- src/camibank/Conta.java
+++ src/camibank/Conta.java
@@ -5,6 +5,17 @@ public class Conta {
 	private int numero;
 	private int agencia;
 	private Cliente titular;
+	private static int total;
+	
+	public Conta(int numero, int agencia) {
+		total++; //Sempre que uma instancia da classe Conta for iniciada, será adicionado +1 ao total
+		if(numero <= 0 || agencia <=0) {
+			System.err.println("Número e agência não podem ser menor ou igual a 0");
+			return;
+		}
+		this.numero = numero;
+		this.agencia = agencia;
+	}
 
 	public void deposita(double valor) {
 		if (valor > 0) {
@@ -37,16 +48,8 @@ public class Conta {
 		return this.numero;
 	}
 
-	public void setNumero(int numero) {
-		this.numero = numero;
-	}
-
 	public int getAgencia() {
 		return this.agencia;
-	}
-
-	public void setAgencia(int agencia) {
-		this.agencia = agencia;
 	}
 
 	public Cliente getTitular() {
@@ -55,5 +58,9 @@ public class Conta {
 
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
+	}
+	
+	public static int getTotal() {
+		return Conta.total;
 	}
 }
